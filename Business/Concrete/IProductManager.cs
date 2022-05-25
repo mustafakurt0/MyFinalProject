04c603f9916f7fcfd6632565fab232d5353a92f1
MUSTAFA KURT <20200855019@ogr.akdeniz.edu.tr>
2022-05-25 20:22:56 +0300

    Add project files.

---
+++ Business/Concrete/IProductManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public record IProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public IProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //İş kodları
+            return _productDal.GetAll();
+
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _productDal.GetAllByCategory(categoryId);
+
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InMemoryProductDal inMemoryProductDal = new InMemoryProductDal();
+            IProductManager productManager = new IProductManager(inMemoryProductDal);
+
+            while (true)
+            {
+                Console.WriteLine("****************************");
+                Console.WriteLine("İşlem");
+                string input = Console.ReadLine();
+                if (input == "get")
+                {
+                    foreach (Product product in productManager.GetAll())
+                    {
+                        Console.WriteLine(product.ProductName);
+                    }
+                }
+                else if (input =="")
+                {
+                    Console.WriteLine("Balance  : 18");
+                }
+
+                else if (input != "get" )
+                {
+                    int categoryId = int.Parse(input);
+                    foreach (Product product in productManager.GetAllByCategory(categoryId))
+                    {
+                        Console.WriteLine(product.ProductName);
+                    }
+                }
+            }
+
+        }
+    }
+}
